131f5b3f16bcfc649fd6ca9f9f1af61f303c3862
doinel1a <doinel1atanasiu@gmail.com>
2025-12-06 16:35:16 +0100

    chore: install & configure prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import sonarjs from "eslint-plugin-sonarjs";
-import unicorn from "eslint-plugin-unicorn";
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import sonarjs from 'eslint-plugin-sonarjs';
+import unicorn from 'eslint-plugin-unicorn';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
-    ignores: ["dist"],
+    ignores: ['dist']
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node }
   },
+  prettierConfig,
+  prettierPlugin,
   sonarjs.configs.recommended,
   unicorn.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommended
 ]);
