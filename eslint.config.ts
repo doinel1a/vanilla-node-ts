7beb8e882f81d18adee53a8c7025a669a9bb1a8a
doinel1a <doinel1atanasiu@gmail.com>
2026-03-03 18:49:45 +0100

    fix: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,13 @@
-import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier/flat';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
 import sonarjs from 'eslint-plugin-sonarjs';
 import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
     ignores: ['dist']
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node }
   },
   prettierConfig,
   prettierPlugin,
