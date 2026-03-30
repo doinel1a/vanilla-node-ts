60695f72f3fee91fbb09cf306facd6dd2696fcc3
doinel1a <doinel1atanasiu@gmail.com>
2026-03-30 20:06:05 +0200

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
 import prettierConfig from 'eslint-config-prettier/flat';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
-import sonarjs from 'eslint-plugin-sonarjs';
+import { configs as sonarjs } from 'eslint-plugin-sonarjs';
 import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
@@ -9,13 +9,75 @@ export default defineConfig([
   {
     ignores: ['dist']
   },
+  sonarjs.recommended,
+  unicorn.configs.recommended,
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true
+      }
+    }
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' }
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'typeLike', format: ['PascalCase'] },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow'
+        },
+        { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow' }
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } }
+      ],
+      '@typescript-eslint/no-restricted-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowNumber: true,
+          allowBoolean: true
+        }
+      ],
+      '@typescript-eslint/strict-boolean-expressions': [
+        'error',
+        {
+          allowNullableBoolean: true,
+          allowNullableNumber: true
+        }
+      ],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off'
+    }
+  },
   prettierConfig,
   prettierPlugin,
-  sonarjs.configs.recommended,
-  unicorn.configs.recommended,
-  tseslint.configs.recommended,
   {
     rules: {
+      curly: ['error', 'all'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': 'off', // Covered by @typescript-eslint
+
       'unicorn/prevent-abbreviations': [
         'error',
         {
@@ -23,7 +85,20 @@ export default defineConfig([
             env: true
           }
         }
-      ]
+      ],
+
+      'sonarjs/deprecation': 'off', // Covered by @typescript-eslint
+      'sonarjs/no-array-delete': 'off', // Covered by @typescript-eslint
+      'sonarjs/no-unused-vars': 'off', // Covered by @typescript-eslint
+      'sonarjs/prefer-regexp-exec': 'off', // Covered by @typescript-eslint
+      'sonarjs/unused-import': 'off', // Covered by @typescript-eslint
+
+      'unicorn/no-array-sort': 'off',
+      'unicorn/no-nested-ternary': 'off', // Covered by sonarjs/no-nested-conditional
+      'unicorn/no-null': 'off',
+      'unicorn/no-useless-undefined': 'off',
+      'unicorn/prefer-includes': 'off', // Covered by @typescript-eslint
+      'unicorn/prefer-string-starts-ends-with': 'off' // Covered by @typescript-eslint
     }
   }
 ]);
